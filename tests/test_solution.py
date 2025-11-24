2cadb2b52618246134b6064708d07c797423ebce
NoFate35 <vsokol35@mail.ru>
2025-11-24 11:48:16 +0300

    Changes to be committed: 	new file:   .gitignore 	new file:   Makefile 	new file:   __init__.py 	new file:   poetry.lock 	new file:   pyproject.toml 	new file:   solution.py 	new file:   tests/__init__.py 	new file:   tests/fixtures/database.custom.json 	new file:   tests/fixtures/database.development.json 	new file:   tests/fixtures/database.preproduction.json 	new file:   tests/fixtures/database.production.json 	new file:   tests/test_solution.py

---
+++ tests/test_solution.py
@@ -0,0 +1,44 @@
+from solution import DatabaseConfigLoader
+from pathlib import Path
+import pytest
+
+
+@pytest.fixture(scope='module')
+def loader():
+    path = Path(__file__).parent / 'fixtures'
+    loader = DatabaseConfigLoader(path)
+    return loader
+
+
+def test_config_loader(loader):
+    expected = {
+        'host': 'google.com',
+        'username': 'postgres',
+    }
+    assert loader.load('production') == expected
+
+
+def test_config_loader2(loader):
+    expected = {
+      'username': 'mysupername',
+    }
+    assert loader.load('custom') == expected
+
+
+def test_config_loader3(loader):
+    expected = {
+      'host': 'localhost',
+      'username': 'postgres',
+      'port': 5000,
+    }
+    assert loader.load('development') == expected
+
+
+def test_config_loader4(loader):
+    expected = {
+      'host': 'dev.server',
+      'username': 'postgres',
+      'port': 5000,
+      'password': 'admin',
+    }
+    assert loader.load('preproduction') == expected
