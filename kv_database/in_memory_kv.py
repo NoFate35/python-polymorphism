7da939ac5d67dde41ccfa3b3a98eb3c837331143
NoFate35 <vsokol35@mail.ru>
2025-11-24 18:56:19 +0300

    Changes to be committed: 	modified:   kv_database/in_memory_kv.py 	modified:   kv_database/solution.py

--- kv_database/in_memory_kv.py
+++ kv_database/in_memory_kv.py
@@ -1,7 +1,26 @@
 import copy
 
 
+
 # BEGIN (write your solution here)
 class InMemoryKV():
-    pass
+    def __init__(self, data):
+        self.data = copy.deepcopy(data)
+        #print('self.data', self.data, 'self.filepath', self.filepath)
+    
+    def set_(self, key, value):
+        self.data.update({key: value})
+
+
+    def unset_(self, key):
+        self.data.pop(key)
+
+
+    def get_(self, key, default=None):
+        return self.data.get(key, default)
+
+    def to_dict(self):
+        return copy.deepcopy(self.data)
+
+    
 # END
--- kv_database/solution.py
+++ kv_database/solution.py
@@ -1,4 +1,11 @@
 # BEGIN (write your solution here)
-def swap_key_value():
-    pass
+def swap_key_value(data):
+    iter_data = data.to_dict()
+
+    for key in iter_data:
+        data.unset_(key)
+    
+    for key, value in iter_data.items():
+        data.set_(value, key)
+    
 # END
