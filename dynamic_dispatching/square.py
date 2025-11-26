05d836bb1c49d6800a23cddad3d22fa5648154af
NoFate35 <vsokol35@mail.ru>
2025-11-26 18:51:54 +0300

    Changes to be committed: 	modified:   dynamic_dispatching/square.py
    	"dynamic_dispatching" finished

--- dynamic_dispatching/square.py
+++ dynamic_dispatching/square.py
@@ -2,5 +2,14 @@ from dispatcher import defmethod
 
 
 # BEGIN (write your solution here)
+def init():
+    print('init__square', __name__)
+    defmethod(__name__, 'get_area', lambda self:  self['data']['side'] * self['data']['side'])
 
+def make(side):
+    print('makeee square', __name__)
+    return {'name': __name__, 'data': {'side': side}}
+
+def get_circle_radius(self):
+    return self['data']['side']
 # END
